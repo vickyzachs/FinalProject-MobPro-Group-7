82d79277f265684cdf2f7bd490b8528bed083215
Clifford D. R. Mantow <s21810434@student.unklab.ac.id>
2021-04-21 19:47:04 +0800

    Testing repo

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>UKost</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>U-KOST</Text>
+      <Text>Universitas Klabat Kost</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '700',
+  },
+});
