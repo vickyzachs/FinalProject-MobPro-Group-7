1c5a989534b9acb0ab916b99180383b72f5ca13e
MobileAppProgramming <s21810001@student.unklab.ac.id>
2021-04-21 18:33:38 +0800

    Init Project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>UKost</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
