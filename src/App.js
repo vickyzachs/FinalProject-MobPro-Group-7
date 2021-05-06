cc531f1a5d7fff96f55e1c75406578e314b9c86b
aldiock <diochristo05@gmail.com>
2021-05-06 10:47:15 +0800

    Slicing HomeScreenMitra

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
-import { AddKost } from './pages';
+import { AddKost, HomeScreenMitra } from './pages';
 
 
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //  <Router />
-    //  <FlashMessage position="top"/>
-    // </NavigationContainer>
-    <AddKost/>
+    <NavigationContainer>
+     <Router />
+     <FlashMessage position="top"/>
+    </NavigationContainer>
+    //<HomeScreenMitra/>
+    //<AddKost/>
   );
 };
 
